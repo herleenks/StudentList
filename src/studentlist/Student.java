d69992b2bfcdee361dc2c9509f05eac73ea153f2
sambh <sambh@10.48.8.103>
2024-09-16 09:30:56 -0400

    Student.java is updated

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -10,7 +10,8 @@ package studentlist;
  */
 public class Student {
     private String name;
-    
+    private String studentID;
+    private String address; 
 
     public String getName() {
         return name;
