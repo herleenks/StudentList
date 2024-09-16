9a302684fa4423f51cc470cd004cc6d5284efb69
sambh <sambh@10.48.8.103>
2024-09-16 10:38:21 -0400

    PartTimeStudent.java filed added

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author sambh
+ */
+public class PartTimeStudent extends Student{
+    
+    private int numberCourses; 
+
+    public int getNumberCourses() {
+        return numberCourses;
+    }
+
+    public void setNumberCourses(int numberCourses) {
+        this.numberCourses = numberCourses;
+    }
+    
+    
+}
